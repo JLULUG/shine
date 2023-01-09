6bc0baa19f88504444786785b71ccb16594ced10
TechCiel <i@ciel.dev>
2023-01-10 02:34:32 +0800

    feat(helpers/earliest): choose earliest schedule among functions

--- shine/helpers/__init__.py
+++ shine/helpers/__init__.py
@@ -4,5 +4,6 @@ from .demo import Demo
 from .exit0 import Exit0
 from .interval import Interval
 from .cron import Cron
+from .earliest import Earliest
 
-__all__ = ['Rsync', 'System', 'Demo', 'Exit0', 'Interval', 'Cron']
+__all__ = ['Rsync', 'System', 'Demo', 'Exit0', 'Interval', 'Cron', 'Earliest']
---
+++ shine/helpers/earliest.py
@@ -0,0 +1,14 @@
+import typing as t
+
+from ..daemon import Task
+
+
+def Earliest(
+    *f: t.Callable[[Task], int]
+) -> t.Callable[[Task], int]:
+    """Use the earliest schedule of multiple functions"""
+    def nxt(self: Task) -> int:
+        return min(x(self) for x in f)
+
+    nxt.__doc__ = f'Earliest({", ".join(x.__doc__ or str(x) for x in f)})'
+    return nxt
